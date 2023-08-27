abcebd035c2800e23cf1501174224532a10a0c13
aqibamir <aqibamirk@gmail.com>
2023-08-27 23:55:20 +0500

    Crawler stays on the same url

--- src/crawler.ts
+++ src/crawler.ts
@@ -10,23 +10,23 @@ export type Page = {
   url: string,
   text: string,
   title: string,
-}
+};
+
 class Crawler {
   pages: Page[] = [];
   limit: number = 1000;
   urls: string[] = [];
-  spider: Spider | null = {};
+  spider: Spider | null = null;
   count: number = 0;
   textLengthMinimum: number = 200;
 
   constructor(urls: string[], limit: number = 1000, textLengthMinimum: number = 200) {
     this.urls = urls;
-    this.limit = limit
-    this.textLengthMinimum = textLengthMinimum
-
-    this.count = 0
+    this.limit = limit;
+    this.textLengthMinimum = textLengthMinimum;
+    this.count = 0;
     this.pages = [];
-    this.spider = {}
+    this.spider = null;
   }
 
   handleRequest = (doc: any) => {
@@ -39,7 +39,7 @@ class Crawler {
     const title = $("title").text() || $(".article-title").text();
     const html = $("body").html();
     const text = turndownService.turndown(html);
-    console.log("crawling ", doc.url)
+    console.log("crawling ", doc.url);
     const page: Page = {
       url: doc.url,
       text,
@@ -48,25 +48,10 @@ class Crawler {
     if (text.length > this.textLengthMinimum) {
       this.pages.push(page);
     }
-
-
-    doc.$("a").each((i: number, elem: any) => {
-      var href = doc.$(elem).attr("href")?.split("#")[0];
-      var targetUrl = href && doc.resolve(href);
-      // crawl more
-      if (targetUrl && this.urls.some(u => {
-        const targetUrlParts = parse(targetUrl);
-        const uParts = parse(u);
-        return targetUrlParts.hostname === uParts.hostname
-      }) && this.count < this.limit) {
-        this.spider.queue(targetUrl, this.handleRequest);
-        this.count = this.count + 1
-      }
-    });
   };
 
   start = async () => {
-    this.pages = []
+    this.pages = [];
     return new Promise((resolve, reject) => {
       this.spider = new Spider({
         concurrent: 5,
@@ -78,20 +63,22 @@ class Crawler {
         keepAlive: false,
         error: (err: any, url: string) => {
           console.log(err, url);
-          reject(err)
+          reject(err);
         },
-        // Called when there are no more requests
         done: () => {
-          resolve(this.pages)
+          resolve(this.pages);
         },
         headers: { "user-agent": "node-spider" },
         encoding: "utf8",
       });
       this.urls.forEach((url) => {
-        this.spider.queue(url, this.handleRequest);
+        if (this.count < this.limit) {
+          this.spider!.queue(url, this.handleRequest);
+          this.count++;
+        }
       });
-    })
-  }
+    });
+  };
 }
 
 export { Crawler };
